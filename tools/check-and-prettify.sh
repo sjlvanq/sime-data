3ae5ae3ac0f6a3b34d528aff9c4331a685de956f
sjlvanq <sjlvanq@gmail.com>
2026-03-08 03:14:26 -0300

    refactor: parallelize file processing in check-and-prettify script

--- tools/check-and-prettify.sh
+++ tools/check-and-prettify.sh
@@ -7,14 +7,25 @@ fi
 
 basedir="$1"
 
-find "$basedir" -name "*.json" -type f | while read -r file; do
+function process_file() {
+    local file="$1"
+    local tmp_err=$(mktemp) #Evita colisiones
     printf "."
-    if jq . "$file" > "$file.tmp" 2> error.log; then
+    if jq . "$file" > "$file.tmp" 2> "$tmp_err"; then
         mv "$file.tmp" "$file"
-    else
-        echo "FALLO en: $file" >> check-and-prettify.log
-        cat error.log >> check-and-prettify.log
+    else 
+        {
+            echo "FALLO en: $file"
+            cat "$tmp_err"
+        } >> check-and-prettify.log
         rm -f "$file.tmp"
     fi
-done
+    rm -f "$tmp_err"
+}
+export -f process_file
+
+echo "Procesando archivos JSON en '$basedir'..."
+find "$basedir" -name "*.json" -type f -print0 | \
+        xargs -0 -I {} -P $(nproc) bash -c 'process_file "$@"' _ {}
+
 printf "\nFinalizado %s\n" "$(date)"
